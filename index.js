16741643f58e234dacc203b9b52f892cd8703a96
yunjinchoi <yunjinchoi@mobinoma.com>
2022-08-23 22:05:43 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5001
+const bodyParser = require('body-parser')
+
+const config = require("./config/key")
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://root:1111@yunjinchoi.ltc9y3z.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     // useNewUrlParser: true, userUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worldgggggggggggg!')
 })
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
